2518d11f4e4f86561a4f71c0bbd675e45d9e69ab
VladimirArtjukh <vladimir.artjukh@gmail.com>
2021-01-15 16:57:44 +0200

    Install Laravel and basic packages

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <i class="fab fa-500px"></i>
+        </div>
+    </div>
+@endsection
